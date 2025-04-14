84c42bb8d7836f1693d61df7152117d05a15c525
Vikas Vimal <vikas.vimal@builder.ai>
2025-04-15 00:48:14 +0530

    chore: initial Rails project setup

---
+++ app/lib/string_calculator.rb
@@ -0,0 +1,5 @@
+class StringCalculator
+  def add(numbers)
+    # Empty for now
+  end
+end
---
+++ spec/lib/string_calculator_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe StringCalculator do
+  # Test for empty string will go here later
+end
