c34526ddd7a0a9f64837c1219b135961707bbbe7
Vikas Vimal <vikasvimal1999@gmail.com>
2025-04-16 11:35:03 +0530

    feat: support unknown number of comma-separated numbers

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -7,5 +7,21 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(calculator.add('')).to eq(0)
     end
+
+    context 'unknown amount of numbers' do
+      it 'returns the number for a single number string' do
+        expect(calculator.add('1')).to eq(1)
+      end
+      
+      it 'returns the sum of two numbers separated by a comma' do
+        expect(calculator.add('1,2')).to eq(3)
+      end
+
+      it 'returns the sum of an unknown amount of numbers' do
+        expect(calculator.add('1,2,3')).to eq(6)
+        expect(calculator.add('5,5,5,5')).to eq(20)
+        expect(calculator.add('100')).to eq(100)
+      end
+    end
   end
 end
